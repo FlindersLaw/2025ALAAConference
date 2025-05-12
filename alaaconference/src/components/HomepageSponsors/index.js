8b61d05c9218079f49d50995b04d9ae2c737979c
Mark Ferraretto <mark@ferraretto.com>
2025-05-12 12:17:39 +0930

    Added College of Law and Lexis sponsor logos.

--- alaaconference/src/components/HomepageSponsors/index.js
+++ alaaconference/src/components/HomepageSponsors/index.js
@@ -19,11 +19,17 @@ const SponsorLogos = [
   {
     thumbnail: 'img/sponsor_logos/cup.png',
   },
+  {
+    thumbnail: 'img/sponsor_logos/college_of_law.png',
+  },
+  {
+    thumbnail: 'img/sponsor_logos/lexisnexis.jpg',
+  },
 ];
 
 function Feature({thumbnail, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--3')}>
       <div className="text--center">
         <img className={styles.SponsorfeatureSvg} role="img" src={thumbnail} />
       </div>
